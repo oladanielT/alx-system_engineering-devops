c815d86df35063d1dadbabbf447de5fe14e50e91
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-08-14 08:20:33 -0400

    function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """
-function to return a request from reddit api
+Function that queries the Reddit API and returns the number of subscribers.
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    module to return number of subscribers if exists and 0 if not
+    Return the number of subscribers for a given subreddit.
+    If the subreddit does not exist, return 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
-
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
+            # Correctly check for keys in the JSON response
             if 'data' in data and 'subscribers' in data['data']:
-                return data["data"]["subscribers"]
-            return 0
+                return data['data']['subscribers']
         return 0
     except requests.exceptions.RequestException:
+        # Return 0 if there's a request error
         return 0
-    
